468576a5b25b2d8c06a9ce842c148df31a0c0abc
Adam Ho <kod.adam.ho@gmail.com>
2015-12-08 15:51:24 -0500

    Testing download.java

--- BitTorrentView.java
+++ BitTorrentView.java
@@ -1,8 +1,34 @@
 //Imports
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JToggleButton;
+import javax.swing.ListSelectionModel;
+import javax.swing.JList;
+import javax.swing.JProgressBar;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.net.URL;
+import java.text.DecimalFormat;
+import java.awt.event.ActionEvent;
+import java.awt.Dimension;
+import java.awt.Component;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.ImageIcon;
+import javax.swing.border.EmptyBorder;
 import java.util.HashMap;
 import java.nio.file.Paths;
 import java.nio.file.Path;
@@ -12,153 +38,276 @@ import javax.swing.*;
 class BitTorrentView extends 	JFrame
 {
 	// Instance attributes used in this example
-	private	JPanel		centerPanel;
-	private	JTable		table;
-	private	JScrollPane scrollPane;
-	private static File file = null;
-	JProgressBar pbar;
-	private static HashMap<Integer,RUBTClient> clients = new HashMap<Integer, RUBTClient>();
-	
-
-	static final int MY_MINIMUM = 0;
-
-	static final int MY_MAXIMUM = 100;
-	
+	public JFileChooser fc;
+	protected File file;
+	protected JTextArea log;
 	static private final String newline = "\n";
+	JFrame frame;
+	private JTable table;
+	MyProgressBar pro;
+	MyTableModel tableModel;
+	// Create some data
+	Object dataValues[] = { "", "","", "", "","", "", "", "" };
+	DecimalFormat twoDForm = new DecimalFormat("#.##");
+	private static HashMap<Integer,RUBTClient> clients = new HashMap<Integer, RUBTClient>();
 
-	JFileChooser fc;
-	JTextArea log;
-	JScrollPane logScrollPane;
 	// Constructor of main frame
-	public BitTorrentView()
-	{
-		// Set the frame characteristics
-		setTitle( "ZMJ BitTorrent" );
-		setBackground( Color.gray );
-
-		JPanel topPanel = new JPanel();
-		getContentPane().add(topPanel, BorderLayout.NORTH);
+	public BitTorrentView() {
+		setResizable(false);
+		getContentPane().setSize(new Dimension(450, 300));
+		setSize(new Dimension(750, 355));
+		setTitle("ZMJTorrent");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initialize();
+	}
+	
+	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setPreferredSize(getPreferredSize());
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JButton btnBrowse = new JButton("Browse");
+	    pro = new MyProgressBar();
+	    pro.setMaximum(100);
+	    // pro.setv
+	    pro.setValue(0);
+	    pro.setBorderPainted(true);
 		
-		log = new JTextArea(5,80);
-        log.setMargin(new Insets(5,5,5,5));
-        log.setEditable(false);
-        logScrollPane = new JScrollPane(log);
-        
+	    dataValues[4]=pro;
+	    
+		JButton btnBrowse = new JButton("");
+		btnBrowse.setSelected(true);
+		btnBrowse.setIcon(new ImageIcon("C:\\Users\\MANISH\\workspace\\GUIDemo\\Open16.gif"));
+		btnBrowse.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnBrowse.setAlignmentX(Component.CENTER_ALIGNMENT);
 		//Create a file chooser
         fc = new JFileChooser();
 		btnBrowse.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-	            int returnVal = fc.showOpenDialog(BitTorrentView.this);
-	            
+			public void actionPerformed(ActionEvent e) {
+				//do stuff when browse btn clicked
+				int returnVal = fc.showOpenDialog(frame);
 
 	            if (returnVal == JFileChooser.APPROVE_OPTION) {
 	                file = fc.getSelectedFile();
-	                System.out.println(file.getName());
-	        		RUBTClient client = new RUBTClient(file);
+	                dataValues[0]=file.getName();
+	                Download client = new Download(file);
 	        		try{
-	        			client.run();
-	        		}catch(Exception e)
+	        			(new Thread(client)).run();
+	        		}catch(Exception a)
 	        		{
-	        			e.printStackTrace();
+	        			a.printStackTrace();
 	        		}
-	                //This is where a real application would open the file.
+	                table.setValueAt(dataValues[0], 0, 0);
+	                dataValues[1]=twoDForm.format(file.length()/1024);
+	                table.setValueAt(dataValues[1], 0, 1);
+	                System.out.println(dataValues[0]);
 	                log.append("Opening: " + file.getName() + "." + newline);
+	                
 	            } else {
 	                log.append("Open command cancelled by user." + newline);
 	            }
-	            //log.setCaretPosition(log.getDocument().getLength());
-
-	        //Handle save button action.
-	        }
-		});
-		topPanel.add(btnBrowse);
-		
-		JButton btnStart = new JButton("Start");
-		btnStart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		topPanel.add(btnStart);
+		getContentPane().setLayout(null);
+		btnBrowse.setBounds(10, 11, 40, 25);
+		getContentPane().add(btnBrowse);
 		
-		JButton btnPause = new JButton("Pause");
-		btnPause.addActionListener(new ActionListener() {
+		final JToggleButton tglbtnStart = new JToggleButton(new ImageIcon("C:\\Users\\MANISH\\workspace\\GUIDemo\\Start.png"));
+		tglbtnStart.setPreferredSize(new Dimension(40, 33));
+		tglbtnStart.setMinimumSize(new Dimension(40, 33));
+		tglbtnStart.setAlignmentX(Component.CENTER_ALIGNMENT);
+		tglbtnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if(tglbtnStart.isSelected()){
+					
+					update();
+					//tglbtnStart.setText("Pause");
+					tglbtnStart.setIcon(new ImageIcon("C:\\Users\\MANISH\\workspace\\GUIDemo\\Pause.png"));
+					//do stuff when Start is clicked
+				}else{
+					//tglbtnStart.setText("Start");
+					tglbtnStart.setIcon(new ImageIcon("C:\\Users\\MANISH\\workspace\\GUIDemo\\Start.png"));
+					//do stuff when pause clicked
+				}
 			}
 		});
-		topPanel.add(btnPause);
+		tglbtnStart.setBounds(112, 11, 40, 25);
+		getContentPane().add(tglbtnStart);
 		
-		JButton btnStop = new JButton("Stop");
-		btnStop.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		topPanel.add(btnStop);
-		
-		JButton btnRemove = new JButton("Remove");
+		JButton btnRemove = new JButton("");
+		btnRemove.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnRemove.setIcon(new ImageIcon("C:\\Users\\MANISH\\workspace\\GUIDemo\\Delete.png"));
 		btnRemove.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent e) {
+				//do stuff when remove btn is clicked
 			}
 		});
-		topPanel.add(btnRemove);
+		btnRemove.setBounds(148, 11, 40, 25);
+		getContentPane().add(btnRemove);
 		
-		JPanel CenterLeftPanel = new JPanel();
-
-		getContentPane().add(CenterLeftPanel, BorderLayout.WEST);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 216, 714, 93);
+		getContentPane().add(scrollPane);
+		
+		log = new JTextArea();
+		log.setEditable(false);
+		scrollPane.setViewportView(log);
+		log.setBackground(Color.LIGHT_GRAY);
+		
+		JLabel lblLogs = new JLabel("Logs");
+		lblLogs.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogs.setBounds(318, 193, 70, 25);
+		getContentPane().add(lblLogs);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 55, 91, 114);
+		getContentPane().add(scrollPane_1);
 		
 		
 		DefaultListModel model = new DefaultListModel();
 		JList list = new JList(model);
-		
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setBorder(new EmptyBorder(0, 0, 0, 0));
 		model.addElement("Completed (" + 1 +")");
 		model.addElement("Active (" + 2 +")");
 		model.addElement("Inactive (" + 0 +")");
-		CenterLeftPanel.add(list);
+		scrollPane_1.setViewportView(list);
 		
 		
-		// Create a panel to hold all other components
-		centerPanel = new JPanel();
-		centerPanel.setLayout( new BorderLayout() );
-		getContentPane().add( centerPanel );
-
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(111, 55, 613, 114);
+		getContentPane().add(scrollPane_2);
+		
 		// Create columns names
-		String columnNames[] = { "Name", "Size", "Completed", "D Speed", "Time", "Status", "Peers", "Seed", "Uploaded", "U Speed"};
+		String columnNames[] = { "Name", "Size(kb)","D Speed", "Time", "Status","Uploaded", "U Speed", "Peers", "Seed" };
 
-		// Create some data
-		String dataValues[][] =
-		{
-			{ "Phase1.torrent", "234", "67", "123", "43", "Downloading", "2", "5", "25", "20" },
-			{ "Phase2.torrent", "892", "100", "279", "90", "Seeding", "5", "10", "52", "10" },
-			{ "Phase3.torrent", "892", "19", "29", "933", "downloading", "2", "1", "5", "23" }
-		}; 
-
-		// Create a new table instance
-		table = new JTable( dataValues, columnNames );
-		
-		// Add the table to a scrolling pane
-		scrollPane = new JScrollPane( table );
-		centerPanel.add( scrollPane, BorderLayout.CENTER );
-		
-		pbar = new JProgressBar();
-	    pbar.setMinimum(MY_MINIMUM);
-	    pbar.setMaximum(MY_MAXIMUM);
-	    pbar.setStringPainted(true);
-	    centerPanel.add(pbar, BorderLayout.SOUTH);
-		
-		JPanel bottomPanel = new JPanel();
-		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
 			
-		JLabel lblErrorLogs = new JLabel("Logs: ");
-		bottomPanel.add(lblErrorLogs);
+		tableModel = new MyTableModel(columnNames, 0);
+		tableModel.addRow(dataValues);
 		
-		bottomPanel.add(logScrollPane);
+		// Create a new table instance
+		table = new JTable(tableModel);
+		
+		
+		TableColumn column = null;
+		for (int i = 0; i < columnNames.length; i++) {
+		    column = table.getColumnModel().getColumn(i);
+		    if (i==0) {
+		        column.setPreferredWidth(100); //1st column is bigger
+		    } else if(i == 4){
+		    	column.setPreferredWidth(150); //5th column is bigger
+		    }else {
+		        column.setPreferredWidth(50);
+		    }
+		}
+		table.getColumnModel().getColumn(4).setCellRenderer(new MyProgressBar());
+		scrollPane_2.setViewportView(table);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(10, 193, 714, 8);
+		getContentPane().add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(10, 45, 714, 14);
+		getContentPane().add(separator_1);
+
 	}
-	public void updateBar(int newValue) {
-	    pbar.setValue(newValue);
-	  }
+	public void update() {
 	
-	private static void download(String name, int i)
+	    MyProgressBar bar = (MyProgressBar) table.getValueAt(0, 4);
+	    bar.setStringPainted(true);
+				
+	    if(bar.getValue()==100){
+	    	bar.setString("Seeding");
+	    }else{
+	    	bar.setString(bar.getValue());
+	    }
+	    bar.setValue(bar.getValue() + 10);
+	    ((MyTableModel) table.getModel()).fireTableCellUpdated(0, 4);
+
+	    System.out.println(" bar value " + bar.getValue());
+
+	}
+	class MyProgressBar extends JProgressBar implements TableCellRenderer {
+
+	    @Override
+	    public Component getTableCellRendererComponent(JTable table,
+	            Object value, boolean isSelected, boolean hasFocus, int row,
+	            int column) {
+
+	        if (value instanceof JComponent) {
+	            return (JComponent) value;
+	        } else {
+	            return null;
+	        }
+	    }
+
+	    public void setString(int value) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+	    public void setValue(int n) {
+
+	        super.setValue(n);
+	    }
+
+	}
+	private class MyTableModel extends DefaultTableModel {
+
+	    public MyTableModel(Object[] obj, int row) {
+
+	        super(obj, row);
+	    }
+
+		@Override
+	    public boolean isCellEditable(int row, int column) {
+	       //all cells false
+	       return false;
+	    }
+	}
+	
+	// Main entry point for this example
+	public static void main(String[] args)
+	{
+		//before everything... load the history download first 
+
+		File[] files = new File(Paths.get(".").toAbsolutePath().normalize().toString()).listFiles();
+		int index = 1;
+		if(files!=null)
+		{
+			for(File file : files)
+			{
+				if(file.getName().contains("hist.txt")){
+					System.out.println(file.getName());
+					//download(file.getName(),index);
+				}
+				index++;
+			}
+        }else{
+        	System.out.println("\nNo history file.");
+        }
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BitTorrentView window = new BitTorrentView();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});	
+		
+	}
+	
+	/*private static void download(String name, int i)
 	{
 		RUBTClient temp = RUBTClient.loadDownloadHistory(name);
 		clients.put(i, temp);
@@ -168,45 +317,5 @@ class BitTorrentView extends 	JFrame
 		{
 			e.printStackTrace();
 		}
-	}
-	
-	
-	// Main entry point for this example
-	public static void main(String[] args)
-	{
-		//before everything... load the history download first 
-		File[] files = new File(Paths.get(".").toAbsolutePath().normalize().toString()).listFiles();
-		int index = 1;
-		if(files!=null)
-		{
-			for(File file : files)
-			{
-                System.out.println(file.getName());
-				if(file.getName().contains("hist.txt"))
-					download(file.getName(),index);
-				index++;
-			}
-        }else{
-            System.out.println("null");
-        }
-		// Create an instance of the test application
-		final BitTorrentView mainFrame	= new BitTorrentView();
-		mainFrame.setVisible( true );
-		mainFrame.setSize(900, 300);
-		
-		for (int i = MY_MINIMUM; i <= MY_MAXIMUM; i++) {
-		      final int percent = i;
-		      try {
-		        SwingUtilities.invokeLater(new Runnable() {
-		          public void run() {
-		            mainFrame.updateBar(percent);
-		          }
-		        });
-		        java.lang.Thread.sleep(100);
-		      } catch (InterruptedException e) {
-		        ;
-		      }
-		    }
-		
-	}
+	}*/
 }
